5d5aeda4f4cac673505887b4fb1907299d044371
Semen Samosiuk <samasiuk1986@gmail.com>
2022-11-01 21:58:57 +0300

    feat: add solution to tack 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const {readdir} = require('fs/promises');
+
+let newPath = path.join(__dirname, 'secret-folder');
+
+readdir(newPath, {
+		withFileTypes: true
+	})
+	.then((data) => data.forEach(file => {
+
+		const filePath = path.join(newPath, file.name);
+
+		if (file.isFile()) {
+			fs.stat(filePath, (err, stats) => {
+				if (stats) {
+					// console.log(stats.size);
+
+					const extFile = path.extname(filePath);
+					const nameFile = path.basename(filePath, extFile);
+					const sizeFileKB = (stats.size / 1024).toFixed(3);
+
+					return console.log(`${nameFile} - ${extFile.replace('.', '')} - ${sizeFileKB}kb`);
+				}
+			});
+		}
+	}));
